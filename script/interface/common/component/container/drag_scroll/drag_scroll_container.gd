8de87bf65c415abc6e29fe4a3649366b2a5f7717
sisipun <sisipun@gmail.com>
2024-01-28 15:10:14 +0100

    [LevelList] free scenes in lazy scroll

--- script/interface/common/component/container/drag_scroll/drag_scroll_container.gd
+++ script/interface/common/component/container/drag_scroll/drag_scroll_container.gd
@@ -2,8 +2,21 @@ class_name DragScrollContainer
 extends ScrollContainer
 
 
+signal vertical_scroll(old_value, new_value)
+signal horizontal_scroll(old_value, new_value)
+
+
+@export var vertical_scroll_enabled: bool = true
+@export var horizontal_scroll_enabled: bool = true
+
+
 func _gui_input(event: InputEvent) -> void:
 	if event is InputEventScreenDrag:
-		scroll_started.emit()
-		scroll_vertical -= event.relative.y
-		scroll_ended.emit()
+		if vertical_scroll_enabled:
+			var old_value: int = scroll_vertical
+			scroll_vertical -= event.relative.y
+			vertical_scroll.emit(old_value, scroll_vertical)
+		if horizontal_scroll_enabled:
+			var old_value: int = scroll_horizontal
+			scroll_horizontal -= event.relative.x
+			horizontal_scroll.emit(old_value, scroll_horizontal)
--- script/interface/common/component/container/lazy_scroll/lazy_scroll_contaner.gd
+++ script/interface/common/component/container/lazy_scroll/lazy_scroll_contaner.gd
@@ -12,42 +12,80 @@ extends DragScrollContainer
 var _loaded_scenes: Dictionary
 var _current_scene_index: int:
 	set(value):
+		var previous_scene_index = _current_scene_index
 		_current_scene_index = value
-		_lazy_load_scenes(_current_scene_index, _lazy_step)
+		_lazy_load_scenes(previous_scene_index, _current_scene_index, _lazy_step)
 
 
 func _ready() -> void:
 	_loaded_scenes = {}
 	_current_scene_index = 0
 	
-	scroll_ended.connect(_on_scroll_ended)
+	vertical_scroll.connect(_on_vertical_scroll)
 
 
-func _on_scroll_ended() -> void:
-	_select_current_scene()
+func _on_vertical_scroll(old_value: int, new_value: int) -> void:
+	while _change_current_scene(old_value, new_value):
+		pass
 
 
-func _select_current_scene() -> void:
+func _change_current_scene(old_scroll_value: int, new_scroll_value: int) -> bool:
+	var scroll_direction: int = new_scroll_value - old_scroll_value
 	var current_scene: Control = _loaded_scenes[_current_scene_index]
-	while (
-		scroll_vertical + size.y > current_scene.position.y + current_scene.size.y
-		and _packed_scenes.size() > _current_scene_index + 1
+	if (
+		scroll_direction > 0
+		and current_scene.position.y + current_scene.size.y < scroll_vertical + size.y 
+		and _current_scene_index + 1 < _packed_scenes.size()
 	):
 		_current_scene_index += 1
-		current_scene = _loaded_scenes[_current_scene_index]
+		return true
+	elif (
+		scroll_direction < 0
+		and current_scene.position.y >= scroll_vertical + size.y
+		and _current_scene_index > 0
+	):
+		_current_scene_index -= 1
+		return true
+	
+	return false
 
 
-func _lazy_load_scenes(current_index: int, step: int) -> void:
-	for i in range(-step, step + 1):
-		_load_scene(current_index + i)
+func _lazy_load_scenes(previous_index: int, current_index: int, step: int) -> void:
+	var drection: int = current_index - previous_index
+	var load_range = range(current_index - step, current_index + step + 1)
+	var free_ragne = range(0, current_index - step) + range(current_index + step + 1, _packed_scenes.size())
+	if drection < 0:
+		load_range.reverse()
+		free_ragne.reverse()
+	
+	for i in load_range:
+		var scene: Control = _load_scene(i)
+		if scene and drection < 0:
+			_scenes.move_child(scene, 0)
+			scroll_vertical += int(scene.custom_minimum_size.y)
+	
+	for i in free_ragne:
+		var scene: Control = _free_scene(i)
+		if scene and drection >= 0:
+			scroll_vertical -= int(scene.custom_minimum_size.y)
 
 
-func _load_scene(index: int) -> bool:
-	if index < 0 or _packed_scenes.size() <= index or _loaded_scenes.has(index):
-		return false
+func _load_scene(index: int) -> Control:
+	if index < 0 or index >= _packed_scenes.size() or _loaded_scenes.has(index):
+		return null
 	
 	
 	var loaded_scene: Control = _packed_scenes[index].instantiate()
 	_loaded_scenes[index] = loaded_scene
 	_scenes.add_child(loaded_scene)
-	return true
+	return loaded_scene
+
+
+func _free_scene(index: int) -> Control:
+	if index < 0 or index >= _packed_scenes.size() or not _loaded_scenes.has(index):
+		return null
+	
+	var scene: Control = _loaded_scenes[index]
+	scene.queue_free()
+	_loaded_scenes.erase(index)
+	return scene
